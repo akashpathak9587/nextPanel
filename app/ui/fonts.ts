50ff14975bdd2a43239440d05afa63ef410a8b1a
Akash Pathak <akashpathak@Akashs-MacBook-Air.local>
2024-06-03 20:24:08 +0530

    next panel using next.js framework

---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Poppins, Lusitana } from 'next/font/google';
+const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: '400' })
+const lusitana = Lusitana({ subsets: ['latin'], weight: '400' })
+
+export { inter, poppins, lusitana }
